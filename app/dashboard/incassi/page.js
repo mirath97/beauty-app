97b3ede835e9c61ccb2887d2358cba6e25e33a8b
Mirabella Giuseppe <g.mirabella@multicedi.com>
2026-04-30 12:17:53 +0200

    debug incassi

--- app/dashboard/incassi/page.js
+++ app/dashboard/incassi/page.js
@@ -129,7 +129,7 @@ export default function IncassiPage() {
       {/* HEADER */}
       <div className="flex justify-between items-center">
         <h1 className="text-3xl font-bold text-pink-700">
-         INCASSI NUOVO
+         INCASSI DEBUG
         </h1>
 
         <div className="flex gap-2">
